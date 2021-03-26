2b137658f8e397f33648ba8854ea2e5329ed0262
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-03-26 17:22:40 +0100

    inserito ultimo messaggio in lista contatti

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,25 @@ var app = new Vue({
   el: '#root',
   data: {
     contacts: contacts,
+    newContacts: [],
   },
   mounted () {
-    for (var contact in this.contacts) {
-      console.log(contact);
-    }
+    this.contacts.forEach((item, i) => {
+      let lastMsg = item.messages[item.messages.length - 1].text;
+      // console.log(newObj);
+
+      let newItem = {
+        ...item,
+        lastMsg,
+      }
+
+      this.newContacts.push(newItem)
+
+
+    });
+
+
+
+
   }
 })
